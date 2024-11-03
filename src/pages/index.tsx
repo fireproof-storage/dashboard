63f983554a2c012d9e243b8d5d7dea0dc42f9a44
Selem Delul <selem@fireproof.storage>
2024-11-02 20:09:00 -0400

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,8 @@ import { redirect } from "react-router-dom";
 
 export async function loader({ request }) {
   const url = new URL(request.url);
-  console.log(url);
   const state = url.searchParams.get("state");
-  console.log(state);
-  if (state) {
+   if (state) {
     const nextURL = JSON.parse(state).next_url;
     if (nextURL) {
       return redirect(nextURL);
